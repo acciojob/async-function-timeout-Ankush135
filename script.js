d0ea0f7deefb5a63932afc273bec069aea1b89d3
Ankush <168534792+Ankush135@users.noreply.github.com>
2025-12-07 13:06:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const text = document.getElementById("text");
+const delay = document.getElementById("delay");
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+
+function wait(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);   
+    });
+}
+
+async function run() {
+    const userText = text.value;     
+    const userDelay = Number(delay.value);  
+
+    await wait(userDelay);  
+
+    output.innerHTML = userText;  
+}
+
+btn.addEventListener("click", run);
